b00aabd68bdc1853fea51780480fad414b7ce732
mahsa reshadi <mahsa@reshadi.com>
2021-06-14 16:51:49 -0700

    change query method

--- server/src/db/dbquery.ts
+++ server/src/db/dbquery.ts
@@ -12,8 +12,18 @@ export const dbConfig = {
   database: "docarch"
 }
 
+interface InsertResult {
+  insertId: number; //https://github.com/mysqljs/mysql#getting-the-id-of-an-inserted-row
+}
+
+interface DeleteResult {
+  affectedRows: number;//https://github.com/mysqljs/mysql#getting-the-number-of-affected-rows
+}
+
+type SelectResult<T> = T[];
+
 let connection: mysql.Connection;
-export async function query<T>(sql: string, params?: any[], customConnection?: mysql.Connection): Promise<T[]> {
+export async function query<T>(sql: string, params?: any[], customConnection?: mysql.Connection): Promise<T> {
   if (!customConnection && !connection) {
     connection = mysql.createConnection(dbConfig);
     connection.connect(err => {
@@ -23,7 +33,7 @@ export async function query<T>(sql: string, params?: any[], customConnection?: m
     });
   }
   const queryConnection = customConnection ?? connection;
-  return new Promise<T[]>((resolve, reject) => {
+  return new Promise<T>((resolve, reject) => {
     queryConnection.query(sql, params, (err, res) => {
       if (err) {
         console.log(err);
@@ -34,4 +44,16 @@ export async function query<T>(sql: string, params?: any[], customConnection?: m
       resolve(res);
     });
   })
+}
+
+export async function insertInto(sql: string, params?: any[]): Promise<InsertResult> {
+  return query<InsertResult>(sql, params);
+}
+
+export async function deleteFrom(sql: string, params?: any[]): Promise<DeleteResult> {
+  return query<DeleteResult>(sql, params);
+}
+
+export async function selectFrom<T>(sql: string, params?: any[]): Promise<SelectResult<T>> {
+  return query<SelectResult<T>>(sql, params);
 }
