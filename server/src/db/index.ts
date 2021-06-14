8fe75bab4aba067748ac015382d11d6bce749654
mahsa reshadi <mahsa@reshadi.com>
2021-06-14 16:52:40 -0700

    update methods

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,9 +1,16 @@
-import { query } from './dbquery';
-import {User} from "../../../common/src/User";
+import { insertInto, query, selectFrom } from './dbquery';
+import { User } from "../../../common/src/User";
+import { Task } from "../../../common/src/Task";
+import { installDb } from './database-creator';
+
+
+
+installDb();
 
 export async function getUser(username: string, password: string): Promise<User | null> {
   try {
-    const result = await query<User>(`SELECT firstname , lastname , userid FROM user WHERE username = ? AND password = ?`, [username, password]);
+    const result = await selectFrom<User>(
+      `SELECT firstname , lastname , userid FROM user WHERE username = ? AND password = ?`, [username, password]);
 
     if (result.length > 0) {
       return result[0];
@@ -17,9 +24,18 @@ export async function getUser(username: string, password: string): Promise<User 
   }
 }
 
-export async function getTasks(userId: number): Promise<Task[]> {
-  query('SELECT task FROM task WHERE userid = ?', [id], (err, result) => {
-    if (err) console.log(err);
-    console.log(result)
-  });
+export async function getTasks(userId: number): Promise<Task[] | null> {
+  const result = selectFrom<Task>('SELECT task FROM task WHERE userid = ?', [userId]);
+  if ((await result).length > 0) {
+    return result;
+  } else {
+    console.log("no task found");
+    return null;
+  }
+
+}
+
+export async function saveTask(value: (string | number)[][]): Promise<number> {
+  const result = await insertInto('INSERT INTO task (task, userid) VALUES ?', [value]);
+  return result.insertId;
 }
