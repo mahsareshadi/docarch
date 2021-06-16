bed7edda9e27312c5df10c6476d312517ad2626f
mahsa reshadi <mahsa@reshadi.com>
2021-06-16 12:58:40 -0700

    change table name

--- server/src/db/database-creator.ts
+++ server/src/db/database-creator.ts
@@ -10,7 +10,7 @@ function createDb() {
 function createTables() {
     query("CREATE TABLE IF NOT EXISTS user (userid INT AUTO_INCREMENT , username VARCHAR(255) , password INT , firstname VARCHAR(255) , lastname VARCHAR(255) , INDEX (userid))");
     query("CREATE TABLE IF NOT EXISTS task (taskid INT AUTO_INCREMENT PRIMARY KEY, userid INT , task TEXT )");
-    query("CREATE TABLE IF NOT EXISTS file (fileid INT AUTO_INCREMENT , address VARCHAR (500) ,INDEX(fileid))");
+    query("CREATE TABLE IF NOT EXISTS uploadfile (fileid INT AUTO_INCREMENT , address VARCHAR (500) ,INDEX(fileid))");
     query("CREATE TABLE IF NOT EXISTS usersfiles (id INT AUTO_INCREMENT PRIMARY KEY, fileid INT , userid INT , INDEX (fileid,userid) )");
 }
 
