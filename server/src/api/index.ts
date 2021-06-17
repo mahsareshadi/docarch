c5a95c4be0439ba21d02029c2271b8b65acd495b
mahsa reshadi <mahsa@reshadi.com>
2021-06-16 21:48:20 -0700

    complete server api

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -1,4 +1,4 @@
-import { getTasks, getUser, saveTask } from "../db";
+import * as db from "../db";
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
@@ -10,11 +10,12 @@ app.use(cors());
 app.get("/", (req, res) => {
   console.log("this is test");
 })
+
 //START LOGIN
 app.post('/login', async (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
-  const user = await getUser(username, password);
+  const user = await db.getUser(username, password);
   if (user === null){
     res.writeHead(500);
     res.write("user not found")
@@ -27,21 +28,44 @@ app.post('/login', async (req, res) => {
 
 //TASK
 //add task
-app.post('/addTask', (req, res) => {
+app.post('/saveTask', async (req, res) => {
   const task : string = req.body.task;
   const userid : number = req.body.id;
-  let value = [[task, userid]]
-  saveTask(value);
-
+  const taskInsertid = await db.saveTask({task, userid});
+  if (taskInsertid === null ){
+    // res.writeHead(500);
+    res.write("task dosent save");
+  }else{
+    //it is insert id
+    res.writeHead(200).send (taskInsertid);
+  }
+  
 }
 );
 
 //show tasks
-app.get('/getTask', (req, res) => {
-  let id :number = req.userId;
-  getTasks(id);
+app.get('/getTask', async (req, res) => {
+  let id :number = req.body.userid;
+  const usertask = await db.getTasks(id);
+  if (usertask === null){
+    res.write("user has no task")
+  }else{
+    res.writeHead(200).send(usertask);
+  }
 })
 
+//delete task
+app.delete('/deleteTask' , async (req, res)=>{
+  const taskid :number = req.body.taskid; 
+  const userid : number = req.body.userid;
+  const deleteTask = await db.deleteTask({taskid,userid});
+  if (deleteTask=== null){
+    res.write("no task deleted")
+  }
+  else{
+    res.writeHead(200).write("one task deleted");
+  }
+})
 
 //END TASK
 
