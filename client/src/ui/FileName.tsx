ab9f7b7dee415b81fde12212c263087855f5cc00
mahsa reshadi <mahsa@reshadi.com>
2021-07-05 17:30:05 -0700

    show user in files

--- client/src/ui/FileName.tsx
+++ client/src/ui/FileName.tsx
@@ -1,21 +1,28 @@
 import React from 'react'
+import { useState } from 'react';
+// import { Link,Switch ,Route,BrowserRouter as Router} from 'react-router-dom';
 import { File } from '../../../common/src/File'
+import { Users } from './Users';
 
-interface Props{
-  file : File
+interface Props {
+  file: File
 }
-export function Filename({file}:Props) {
-  let href=`"../../../server/Upload/${(file.address).split("fakepath\\")[1]}`
-
+export function Filename({ file }: Props) {
+  const [users,setUsers]=useState("");
+  let href = `"../../../server/Upload/${(file.address).split("fakepath\\")[1]}`
+  
   return (
+    <>
     <div className="todo">
       <li className="todo-item">
         {(file.address).split("fakepath\\")[1]}
       </li>
-      
+
       <a href={href} download>
-      <button className="download-btn">&#8681;</button></a>
-      <button className="send-btn">&#8680;</button>
+        <button className="download-btn">&#8681;</button></a>
+        <button onClick={()=>{setUsers("showuser")}} className="send-btn">&#8680;</button>
     </div>
+    {users === "showuser" && <Users setUsers={setUsers} fileid={file.fileid}/>}
+    </>
   )
 }
